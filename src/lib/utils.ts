21183eb7829c75f9da366034c54188b29fb53456
Nathan Cuevas <nathanacuevas97@gmail.com>
2024-05-22 16:34:44 -0700

    Added a helper function for copying text to the clipboard to a simplified function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -10,3 +10,14 @@ export function cn(...inputs: ClassValue[]) {
 export function getNewScramble(event: CubingEvent) {
   return generateScramble({ type: event });
 }
+
+export function copyToClipboard(text: string) {
+  navigator.clipboard
+    .writeText(text)
+    .then(() => {
+      console.log("copied");
+    })
+    .catch(() => {
+      throw new Error("Something went wrong");
+    });
+}
